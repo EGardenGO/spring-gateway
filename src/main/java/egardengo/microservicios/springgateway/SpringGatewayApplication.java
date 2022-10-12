a5f7c2786cf3957394839415fde029f2f6dcd797
joffre <joffre.hermosilla@gmail.com>
2022-10-12 11:59:57 -0500

    chore[1] configuracion application.yml incluision dependencias bootstrap aop

--- src/main/java/egardengo/microservicios/springgateway/SpringGatewayApplication.java
+++ src/main/java/egardengo/microservicios/springgateway/SpringGatewayApplication.java
@@ -2,7 +2,9 @@ package egardengo.microservicios.springgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringGatewayApplication {
 
